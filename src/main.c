ae31fd34db1ed0120322677e45bb1a2f8a855583
YiraSan <yirasan.bk@gmail.com>
2024-12-01 18:11:19 +0100

    add appstate

--- src/main.c
+++ src/main.c
@@ -1,17 +1,27 @@
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL.h>
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+typedef struct {
+    
+} appstate_t;
+
+
+SDL_AppResult SDL_AppInit(void **as, int argc, char **argv) {
+    *as = SDL_malloc(sizeof(appstate_t));
+    appstate_t *appstate = (appstate_t*) as;
 
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void *appstate) {
+SDL_AppResult SDL_AppIterate(void *as) {
+    appstate_t *appstate = (appstate_t*) as;
 
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+SDL_AppResult SDL_AppEvent(void *as, SDL_Event *event) {
+    appstate_t *appstate = (appstate_t*) as;
 
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;
@@ -20,6 +30,8 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+void SDL_AppQuit(void *as, SDL_AppResult result) {
+    appstate_t *appstate = (appstate_t*) as;
 
+    SDL_free(appstate);
 }
