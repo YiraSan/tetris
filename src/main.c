0758eac325cbfd2c2a8813a702380776810d08b2
YiraSan <yirasan.bk@gmail.com>
2024-12-01 17:51:29 +0100

    handle quit event (ctrl+c heaven)

--- src/main.c
+++ src/main.c
@@ -12,7 +12,11 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
-    
+
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+
     return SDL_APP_CONTINUE;
 }
 
